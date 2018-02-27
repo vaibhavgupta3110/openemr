8d2820070a477abf72495229fcd4c46975600da8
vaibhavgupta3110 <vaibhav.gupta@me.com>
2018-02-27 14:20:07 +0530

    poof

--- setupscript.sh
+++ setupscript.sh
@@ -7,35 +7,48 @@ set -x
 sudo apt-get update
 sudo apt-get -y upgrade
 
-sudo apt-get -y -f install build-essential apache2 git tar curl python openssl git libffi-dev python-dev vim nano bash bash-doc bash-completion tree curl apache2 mariadb-server libapache2-mod-php libtiff-tools php php-mysql php-cli php-gd php-xsl php-curl php-mcrypt php-soap php-json php-gettext imagemagick php-mbstring php-zip php-ldap php-xml
+sudo apt-get -y -f install build-essential apache2 git tar curl python openssl git libffi-dev python-dev vim nano bash bash-doc bash-completion tree curl apache2 mariadb-server libapache2-mod-php libtiff-tools php php-mysql php-cli php-gd php-xsl php-curl php-mcrypt php-soap php-json php-gettext imagemagick php-mbstring php-zip php-ldap php-xml libssl-dev w3m 
 
 #run mariadb commands for permissions
 
-touch ~/sqlrunner.sql
+sudo touch ~/sqlrunner.sql
 
-echo "use mysql;" > ~/sqlrunner.sql
-echo "update user set plugin='' where User='root';" >> ~/sqlrunner.sql
-echo "flush privileges;" >> ~/sqlrunner.sql
-echo "\q" >> ~/sqlrunner.sql
+sudo echo "use mysql;" > ~/sqlrunner.sql
+sudo echo "update user set plugin='' where User='root';" >> ~/sqlrunner.sql
+sudo echo "flush privileges;" >> ~/sqlrunner.sql
+sudo echo "\q" >> ~/sqlrunner.sql
 
 sudo mysql -u root < ~/sqlrunner.sql
-# use mysql;
-# update user set plugin='' where User='root';
-# flush privileges;
-# \q
+
+#use mysql;
+#update user set plugin='' where User='root';
+#flush privileges;
+#\q
 
 sudo service apache2 restart
 sudo service mysql restart
 
-#sudo rm -r /var/www/html
+sudo rm -r /var/www/html
 
-sudo git clone https://github.com/vaibhavgupta3110/openemr /var/www/openemr
+echo "Which branch?: "
+read BRANCH
+
+sudo git clone https://github.com/vaibhavgupta3110/openemr -b $BRANCH /var/www/html
 
 sudo service apache2 restart
 sudo service mysql restart
 
 sleep 400
 
+echo "Browser opened?: " "(y or n)"
+
+read ANSWER
+
+if [ "$ANSWER" = "y" ]; then
+	continue
+else
+	sleep 400
+fi
 
 # openemr's setup requireemnts
 
